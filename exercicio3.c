daa3d3405ec3fec06e8c15d2be922ddd7c419e3b
LatchedMicrobe0 <rodrigo.pontes.oliveira.lima@gmail.com>
2025-09-29 22:05:37 -0300

    Versão comentada do exercício 3

--- exercicio3.c
+++ exercicio3.c
@@ -14,8 +14,8 @@ void timer0_int();
 void functionA();
 void functionB();
 
-int timer_count = 0;
-int timer_count2 = 0;
+int timer_count = 0; // Contador de tempo (ou seja, do número de interrupções) da função da máquina de estados A
+int timer_count2 = 0; // Contador de tempo (ou seja, do número de interrupções) da função da máquina de estados B
 unsigned char aux_p1 = 0xFF;
 
 void main(){
@@ -27,6 +27,7 @@ void main(){
 	}
 } 
 
+// Função que inicializa o registrador de modo (TMOD) e de controle (TCON) no modo timer, com valores esperados
 void timer0_inicializa(){
 	TR0 = 0; // Desliga o Timer 0
 	TMOD = (TMOD & 0xF0)|0x01; // Timer 0 programado no Modo 1
@@ -36,6 +37,7 @@ void timer0_inicializa(){
 	TR0 = 1; // Habilita a execução do timer 0	 
 }
 
+// Função que implementa o tratamento de interrupções do timer 0 (usando o banco de registradores 2)
 void timer0_int() interrupt 1 using 2{
 	TR0 = 0; // Desliga Timer0
 	TL0 += TL0_Inicial; // Faz recarga da contagem do Timer0
@@ -59,12 +61,14 @@ void functionA(){
 				break;
 			//Estado : bit 1 de P2 em 0
 			case 1: if (index_one){
+				//Zera o contador dessa função antes de entrar no estado do temporizador
 				timer_count = 0;
 				// Mantém os 4 LSB de aux_p1 e substitui os 4 MSB pelos 4 MSB da porta 0 (P0)
 				aux_p1 = (aux_p1 & 0x0f) | (P0 & 0xf0);
 				state = 2;
 			} 
 				break;
+			// Estado: Temporizador de 1s
 			case 2: if (timer_count>=100){
 				// Zera os 4 MSB de aux_p1 e mantém os 4 LSB
 				aux_p1 = aux_p1 & 0x0f;
@@ -91,12 +95,14 @@ void functionB(){
 				break;
 			//Estado : bit 0 de P2 em 0
 			case 1: if (index_zero){
+				//Zera o contador dessa função antes de entrar no estado do temporizador
 				timer_count2 = 0;
 				// Mantém os 4 MSB de aux_p1 e substitui os 4 LSB pelos 4 LSB de P0
 			  aux_p1 = (aux_p1 & 0xf0) | (P0 & 0x0f);
 				state2 = 2;
 			} 
 				break;
+			// Estado: Temporizador de 1s
 			case 2: if (timer_count2>=100){
 				// Zera os 4 LSB de aux_p1 e mantém os 4 MSB
 			  aux_p1 = aux_p1 & 0xf0;
